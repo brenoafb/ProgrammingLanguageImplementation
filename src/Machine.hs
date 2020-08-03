fc646c744414337a8d4d4c045ba6e957f1fafe6d
brenoafb <breno.afb@gmail.com>
2020-08-03 14:32:15 -0300

    Add vm definition

--- src/Machine.hs
+++ src/Machine.hs
@@ -5,8 +5,21 @@ data Op = PUSH Integer
         | DIV
         | ADD
         | SUB
+        | NEG
+        deriving Show
 
 type Program = [Op]
 
-execute :: Program
-execute = undefined
+execute :: [Integer] -> Program -> Maybe Integer
+execute [] [] = Nothing
+execute [x] [] = Just x
+execute _ [] = Nothing
+execute st (PUSH x:ops) = execute (x:st) ops
+execute (x:y:xs) (op:ops) = case op of
+                              MUL -> execute (x * y : xs) ops
+                              DIV -> execute (x `div` y : xs) ops
+                              ADD -> execute (x + y : xs) ops
+                              SUB -> execute (x - y : xs) ops
+                              _   -> Nothing
+execute _ _ = Nothing
+
