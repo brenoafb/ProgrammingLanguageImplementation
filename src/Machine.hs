a635a625f1cc8ed82a73fe03a9cf5d53505ae611
brenoafb <breno.afb@gmail.com>
2020-08-03 15:34:43 -0300

    Fix argument order in sub, div.

--- src/Machine.hs
+++ src/Machine.hs
@@ -11,15 +11,15 @@ data Op = PUSH Integer
 type Program = [Op]
 
 execute :: [Integer] -> Program -> Maybe Integer
-execute [] [] = Nothing
-execute [x] [] = Just x
-execute _ [] = Nothing
+execute [x] [] = Just x -- normal finalization
+execute [] [] = Nothing -- no item in stack upon finalizing
+execute _ [] = Nothing -- more than one item in stack upon finalizing
 execute st (PUSH x:ops) = execute (x:st) ops
 execute (x:y:xs) (op:ops) = case op of
                               MUL -> execute (x * y : xs) ops
-                              DIV -> execute (x `div` y : xs) ops
+                              DIV -> execute (y `div` x : xs) ops
                               ADD -> execute (x + y : xs) ops
-                              SUB -> execute (x - y : xs) ops
+                              SUB -> execute (y - x : xs) ops
                               NEG -> execute ((-x) : y : xs) ops
                               _   -> Nothing
 execute (x:xs) (NEG:ops) = execute ((-x) : xs) ops
