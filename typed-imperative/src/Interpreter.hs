954df5a342a73853e5137402831c15a98fbeb07f
brenoafb <breno.afb@gmail.com>
2020-08-19 23:59:46 -0300

    Use ExceptT in TIM's interpreter, add execStmt and eval cases.

--- typed-imperative/src/Interpreter.hs
+++ typed-imperative/src/Interpreter.hs
@@ -1,8 +1,11 @@
-module Interpreter where
+module Interpreter
+  ( runProgram
+  ) where
 
 import Parser
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Except
 import qualified Data.Map as M
 
 data Value = IntV Int
@@ -13,14 +16,41 @@ data Value = IntV Int
 type Frame = M.Map String Value
 type Env = [Frame]
 
-type Eval a = ReaderT Program (State Env) a
+type Error = String
 
-exec :: Program -> Env
-exec = undefined
+type Eval a = ExceptT Error (ReaderT Program (State Env)) a
+
+runProgram :: Program -> Env
+runProgram program = execState (runReaderT (runExceptT exec) program) env
+  where env = [M.empty]
+
+getMainFunc :: Eval Function
+getMainFunc = do
+  functions <- ask
+  case filter (\(Function name args retType body) -> name == "main") functions of
+    [main] -> return main
+    _ -> throwError "Invalid program: no main function"
+
+exec :: Eval Env
+exec = do
+  (Function _ _ _ mainBody) <- getMainFunc
+  execStmt mainBody
 
 execStmt :: Stmt -> Eval Env
-execStmt = undefined
+execStmt (Decl typ var) = undefined
 
-eval :: Function -> Eval Value
+execStmt (Assn var expr) = undefined
+
+execStmt (If cond conseq) = undefined
+
+execStmt (IfElse cond conseq alt) = undefined
+
+execStmt (Block stmts) = undefined
+
+execStmt (While cond body) = undefined
+
+execStmt (Return expr) = undefined
+
+eval :: Expr -> Eval Value
 eval = undefined
 
